e38b12cae14046bb7d3980494fb192ed9eb50923
Asif-Abdullah21 <asifabdullah214365@gmail.com>
2023-05-07 23:53:42 +0600

    Exact row and column printing

--- 2_Exact_row_column_printing.c
+++ 2_Exact_row_column_printing.c
@@ -1,39 +1,38 @@
+#include <stdio.h>
+int main()
+{
+    int row, col, i, j;
+    scanf("%d %d", &row, &col);
+    int a[row][col];
 
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
 
+    // For printing exact row:
+    printf("\nFor printing exact row:\n");
 
-//From muhammad_sayem's github:
-// #include<stdio.h>
-// int main()
-// {
-//     int row, col, i, j;
-//     scanf("%d %d", &row, &col);
-//     int a[row][col];
+    int exact_row;
+    scanf("%d", &exact_row); // Exact koto tomo row er value dekhte chai sheta input neyar jonno
 
-//     for(i=0; i<row; i++){
-//         for(j=0; j<col; j++){
-//             scanf("%d", &a[i][j]);
-//         }
-//     }
+    for (int j = 0; j < col; j++)
+    {
+        printf("%d ", a[exact_row][j]); //fixed row
+    }
+
+    printf("\n\nFor printing exact column:\n");
+
+    int exact_col; // Exact koto tomo column er value dekhte chai sheta input neyar jonno 
+    scanf("%d", &exact_col);
+
+    for (int i = 0; i < row; i++)
+    {
+        printf("%d\n",a[i][exact_col]); //fixed column
+    }
     
-//     /// For printing exact row ///
-
-//     int exact;              // Exact koto tomo row er value dekhte chai sheta input neyar jonno ///
-//     scanf("%d", &exact);
-
-//     for(i=0; i<col; i++){
-//         printf("%d ", a[exact][i]);
-//     }
-
-//     // For printing exact column ///
-
-//     /*
-//     int exact;              // Exact koto tomo column er value dekhte chai sheta input neyar jonno ///
-//     scanf("%d", &exact);
-
-//     for(i=0; i<row; i++){
-//         printf("%d ", a[i][exact]);
-//     }
-//     */
-    
-//     return 0;
-// }
+    return 0;
+}
